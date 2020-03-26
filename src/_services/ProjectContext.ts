03d672400862f3a96e3040a48f00a270bac80e33
Anthony Ikeda <anthony.ikeda@cognizant.com>
2020-03-26 11:44:00 -0700

    Still not working but storing for reference

--- src/_services/ProjectContext.ts
+++ src/_services/ProjectContext.ts
@@ -1,25 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, FunctionComponent } from 'react';
 
-const loadProject = async (username: string) => {
-    console.log("Loading projects...");
-    const loader = useContext(ProjectContext);
-
-    fetch('http://localhost:8080/v1/projects')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            loader.setProjectList(data);
-            data.forEach((element: string) => {
-                console.log(element);
-            });
-        })
-        .catch(console.error);
-}
+const [projectList, setProjectList] = useState();
 
 const ProjectContext = React.createContext(['']);
 
-export const ProjectProvider = ProjectContext.Provider;
-export const ProjectConsumer = ProjectContext.Consumer;
+const ProjectProvider = () => {
+
+    const [projectList, setProjectList] = useState();
+    
+    const loadProject = async (username: string) => {
+        console.log("Loading projects...");
+        const loader = useContext(ProjectContext);
+    
+        fetch('http://localhost:8080/v1/projects')
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setProjectList(data);
+                data.forEach((element: string) => {
+                    console.log(element);
+                });
+            })
+            .catch(console.error);
+    };
+
+    return (
+        <ProjectContext.Provider
+            value={{
+                projectList,
+                setProjectList
+            }}>
+            {children}
+        </ProjectContext.Provider>
+    );
+};
 
 export default ProjectContext;
+
+export { ProjectProvider };
--- src/battle-grounds/DynamicProjects.tsx
+++ src/battle-grounds/DynamicProjects.tsx
@@ -1,11 +1,6 @@
 import React, { useContext, FunctionComponent } from 'react';
 import ProjectContext from '../_services/ProjectContext';
 
-const loadedProjects = (message: string) => {
-    console.log(message);
-    return "Welcome " + message;
-};
-
 
 type DynamicProjectsProps = {
     username: string,
@@ -13,8 +8,9 @@ type DynamicProjectsProps = {
 
 export const DynamicProjects: FunctionComponent<DynamicProjectsProps> = ({username}) => {
     const loader = useContext(ProjectContext);
+
     return (
-        <ProjectContext.Provider value={loadedProjects(username)}>
+        <ProjectContext.Provider value={loadProjects(username)}>
         <div>{loader}</div>
         </ProjectContext.Provider>
     );
