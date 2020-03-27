60eb69ff1cbac815619d14ea014e462780881372
Anthony Ikeda <anthony.ikeda@cognizant.com>
2020-03-26 17:16:15 -0700

    Styling with material-ui

--- src/battle-grounds/DynamicProjects.tsx
+++ src/battle-grounds/DynamicProjects.tsx
@@ -1,6 +1,12 @@
 import React, { useContext, FunctionComponent, useEffect } from 'react';
 import ProjectContext from '../_services/ProjectContext';
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 type DynamicProjectsProps = {
     username: string,
@@ -15,7 +21,22 @@ export const DynamicProjects: FunctionComponent<DynamicProjectsProps> = ({userna
     
 
     return (
-        <div>{loader?.projectList}</div>
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Focus</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {loader?.projectList?.map(row => (
+                    <TableRow key={row}>
+                        <TableCell component="th" scope="row"><a href="3">{row}</a></TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 };
 
--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,6 +1,21 @@
 import React, { FunctionComponent, useEffect, useState, useContext } from 'react';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import ProjectContext from '../_services/ProjectContext';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+      maxWidth: 700,
+    },
+});
 
 type DashboardProps = {
     username: string,
@@ -10,6 +25,7 @@ const useProjects = () => useContext(ProjectContext);
 export const Dashboard: FunctionComponent<DashboardProps> = ({username}) => {
     const [project, setProject] = useState<string>();
     const projects = useProjects();
+    const classes = useStyles();
 
     useEffect(() => {
         setProject('Loews');
@@ -21,15 +37,22 @@ export const Dashboard: FunctionComponent<DashboardProps> = ({username}) => {
 
     return project ? (
         <div className="Dashboard">
-            <table>
-                <thead>
-                    <tr><th>Welcome {username}</th></tr>
-                    <tr><th>Projects</th></tr>
-                </thead>
-                <tbody>
-                    {projects?.projectList?.map(focus => <tr key={focus}><td>{focus}</td></tr>)}
-                </tbody>
-            </table>
+            <TableContainer className={classes.table} component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Focus</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {projects?.projectList?.map(row => (
+                        <TableRow key={row}>
+                            <TableCell component="th" scope="row"><a href="3">{row}</a></TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
             <Button variant="contained" color="primary" onClick={logProjects}>Hello World</Button>
         </div>
     ) : (
